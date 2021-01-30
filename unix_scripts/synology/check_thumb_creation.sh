60d40f6f612de540b1ed1bc9ab384877773a8a37
paul ashby <ashbyp@yahoo.co.uk>
2021-01-30 10:19:56 +0000

    found the progress file

--- unix_scripts/synology/check_thumb_creation.sh
+++ unix_scripts/synology/check_thumb_creation.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 #
-# Check thumbnail process, needs to be run as root to read /proc
+# Check thumbnail process, needs to be run as root for to read /proc
 
 PID=`pidof synomkthumbd`
 echo "PID of synomkthumbd is $PID"
 
 echo "Open file descriptors"
 ls -l /proc/$PID/fd
+
+if [ -e /var/spool/conv_progress_photo ]; then
+   echo "Progress file"
+else
+   echo "Progress file does not exist"
+fi
+
+more /var/spool/conv_progress_photo
 
 while true; do
    read -p "Restart daemon? " yn
