c77670e761bf38e6404dfd3996443bfa65b7406e
paul ashby <ashbyp@yahoo.co.uk>
2021-01-30 10:33:26 +0000

    fixed file test#

--- unix_scripts/synology/check_thumb_creation.sh
+++ unix_scripts/synology/check_thumb_creation.sh
@@ -8,13 +8,12 @@ echo "PID of synomkthumbd is $PID"
 echo "Open file descriptors"
 ls -l /proc/$PID/fd
 
-if [ -e /var/spool/conv_progress_photo ]; then
+if [ -f "/var/spool/conv_progress_photo" ]; then
    echo "Progress file"
+   cat /var/spool/conv_progress_photo
 else
    echo "Progress file does not exist"
 fi
-
-more /var/spool/conv_progress_photo
 
 while true; do
    read -p "Restart daemon? " yn
