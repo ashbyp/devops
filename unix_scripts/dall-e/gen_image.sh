5edd96ba629676e41a509ef6b4f9aa59b9b8cb0e
ashbyp <ashbyp@yahoo.co.uk>
2022-11-05 11:16:41 +0000

    formatting

--- unix_scripts/dall-e/gen_image.sh
+++ unix_scripts/dall-e/gen_image.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 usage() {
-	echo "$0: <-k|--keep-image> \"description of image for dall-e to consume\""
-	exit -1
+    echo "$0: <-k|--keep-image> \"description of image for dall-e to consume\""
+    exit -1
 }
 
 keep_image=0
@@ -11,28 +11,28 @@ image_description=""
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-    -k|--keep-image)
-	    if [ "$image_description" ]; then
-	    	usage
-	    fi
-	    keep_image=1
-	    shift
-	    ;;
-    *)
-	    if [ "$image_description" ]; then
-            	    image_description="$image_description $1"
-            else
-		    image_description=$1
-	    fi
-
+        -k|--keep-image)
+            if [ "$image_description" ]; then
+                usage
+            fi
+            keep_image=1
             shift
-            ;;
+        ;;
+        *)
+            if [ "$image_description" ]; then
+                image_description="$image_description $1"
+            else
+                image_description=$1
+            fi
+            
+            shift
+        ;;
     esac
 done
 
 
 if [ "$image_description" == "" ]; then
-	usage
+    usage
 fi
 
 echo "Generating image: \"$image_description\""
@@ -42,37 +42,37 @@ loc=$(dirname "$(readlink -f "$0")")"/"
 key=`cat openai_key.txt`
 
 output=`curl https://api.openai.com/v1/images/generations \
-	-H "Content-Type: application/json" \
-	-H "Authorization: Bearer $key" \
-	-d "{
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $key" \
+-d "{
           \"prompt\": \"$image_description\",
 	  \"n\": 1,
 	  \"size\": \"1024x1024\"
-        }"`
+}"`
 
 url=`echo $output | sed  -n 's/.*\(https.*\)" } ] }/\1/p'`
 
 if [ $keep_image -eq 1 ]; then
-
-	if [ -f image4.png ]; then
-		mv image4.png image5.png
-	fi
-	
-	if [ -f image3.png ]; then
-		mv image3.png image4.png
-	fi
-	if [ -f image2.png ]; then
-		mv image2.png image3.png
-	fi
-	if [ -f image1.png ]; then
-		mv image1.png image2.png
-	fi
-	if [ -f image.png ]; then
-		mv image.png image1.png
-	fi
-	curl $url -o image.png
-	url=$loc/image.png
-	echo "Image saved to image,png"
+    
+    if [ -f image4.png ]; then
+        mv image4.png image5.png
+    fi
+    
+    if [ -f image3.png ]; then
+        mv image3.png image4.png
+    fi
+    if [ -f image2.png ]; then
+        mv image2.png image3.png
+    fi
+    if [ -f image1.png ]; then
+        mv image1.png image2.png
+    fi
+    if [ -f image.png ]; then
+        mv image.png image1.png
+    fi
+    curl $url -o image.png
+    url=$loc/image.png
+    echo "Image saved to image,png"
 fi
 
 start chrome $url
